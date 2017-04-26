8cd9cca3156e5d6f829c4481e82716741fe40906
muramatsu mikiya <mikiya@adm.ne.jp>
2017-04-26 20:23:27 +0900

    Add submodules.

---
+++ Axel.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,9 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: Guid("28ce657d-7839-4313-bef2-61b84d0ff06f")]
+[assembly: AssemblyTitle("Axel.Tests")]
+[assembly: AssemblyProduct("Axel.Tests")]
+[assembly: AssemblyCopyright("Copyright © 2017 mik14a <mik14a@gmail.com>")]
+[assembly: ComVisible(false)]
+[assembly: AssemblyVersion("1.0.0.*")]
